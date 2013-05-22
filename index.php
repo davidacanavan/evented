625fc7de053b2975076edd70c5915df02fd6dc69
David Canavan <d.i.dave.canavan@gmail.com>
2013-05-23 00:18:03 +0100

    d

---
+++ index.php
@@ -0,0 +1,123 @@
+
+<!DOCTYPE html>
+<head>
+	<title>Evented</title>
+	<link rel = "stylesheet" href = "style.css"/>
+	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+	<script src="http://code.jquery.com/jquery.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+	<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">Evented</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">For Users</a></li>
+              <li><a href="#about">For Promoters</a></li>
+              <li><a href="#contact">Contact Us!</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+	<div class = "container big-max-width">
+
+		<div class = "jumbotron">
+			<h1>People Unite!</h1>
+			<p class = "lead">
+				Here's some text where i'll be saying various things! Don't you just love these things i'm saying!
+				You should totally check out my blog, it's just totes unrealz! Look at that! A z! even more unrealz!
+			</p>
+		</div>
+
+		<div class="row">
+		    <div class="span3">
+		        <h2>Discover</h2>
+		        <p>
+		        	Use our <span class = "text-success">live wall</span> feature to browse what's on. Find local and upcoming events easier than ever with <span class = "text-success">no advertisements</span> popping up or getting in your way.
+		        </p>
+		        </div>
+		        <div class="span3">
+		            <h2>Share</h2>
+		            <p>
+		            	Feeling nosey? Check out what <span class = "text-success">your friends are going to</span>! Feeling loud? <span class = "text-success">Share events with friends</span> and make them come too!
+		          	</p>
+		        </div>
+		        <div class="span3">
+		          <h2>Get Tickets</h2>
+		          <p>
+		          	 Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+		          </p>
+		        </div>
+		        <div class = "span3">
+		          	<h2>Dance!</h2>
+		          	<p>
+		          		Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+		          	</p>
+		    </div>
+		</div>
+
+		<div class = "jumbotron">
+			<h1>Promoters of the World!</h1>
+			<p class = "lead">
+				Here's some text where i'll be saying various things! Don't you just love these things i'm saying!
+				You should totally check out my blog, it's just totes unrealz! Look at that! A z! even more unrealz!
+			</p>
+		</div>
+
+		<div class="row">
+		    <div class="span3">
+		        <h2>Discover</h2>
+		        <p>
+		        	Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
+		        </p>
+		    </div>
+		    <div class="span3">
+		        <h2>Share</h2>
+		        <p>
+		        	Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. 
+		        </p>
+		    </div>
+		    <div class="span3">
+		        <h2>Entertain</h2>
+		        <p>
+		        	Sell tickets with 
+		        </p>
+		    </div>
+		    <div class = "span3">
+		        <h2>Make Money!</h2>
+		        <p>
+		          	Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.
+		        </p>
+		    </div>
+		</div>
+
+	</div>
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
