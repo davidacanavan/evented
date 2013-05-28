82821bc6719d47992abaf1f6e0a4f0fe57723655
David Canavan <d.i.dave.canavan@gmail.com>
2013-05-28 01:01:45 +0100

    new index.php

--- index.php
+++ index.php
@@ -40,24 +40,28 @@
 		<div class="row">
 		    <div class="span3">
 		        <h2>Discover</h2>
+		        <img class="image-thumbnail" src="images/discover.png" alt="discover"/>
 		        <p>
-		        	Use our <span class = "text-success">live wall</span> feature to browse what's on. Find local and upcoming events easier than ever with <span class = "text-success">no advertisements</span> popping up or getting in your way.
+		        	Browse our <span class = "text-success">live wall</span> feature to discover upcoming events. The Event'd wall is <span class = "text-success">unique for each user</span> and changes relative to your social network.
 		        </p>
 		    </div>
 	        <div class="span3">
 	            <h2>Share</h2>
+		        <img class="image-thumbnail" src="images/share.png" alt="discover"/>
 	            <p>
 	            	Feeling nosey? Check out what <span class = "text-success">your friends are going to</span>! Feeling loud? <span class = "text-success">Share events with friends</span> and make them come too! Feeling really loud? I'm afraid you'll have to leave...
 	          	</p>
 	        </div>
 	        <div class="span3">
 	          	<h2>Get Tickets</h2>
+		        <img class="image-thumbnail" src="images/tickets.png" alt="discover"/>
 	          	<p>
 	          		Use your <span class = "text-success">credit card</span> to secure tickets for you and your friends. Pre-arrange purcheses
 	          	</p>
 	        </div>
 	        <div class = "span3">
 	          	<h2>Enjoy!</h2>
+		        <img class="image-thumbnail" src="images/enjoy.png" alt="discover"/>
 	          	<p>
 	          		<span class = "text-success">Dance! Dance! Dance!</span> We just don't recommend dancing if you go to a play though. When it's all over, <span class = "text-success">put up your pictures</span> and comments for everyone to see!
 	          	</p>
@@ -74,24 +78,28 @@
 		<div class="row">
 		    <div class="span3">
 		        <h2>Create</h2>
+		        <img class="image-thumbnail" src="images/create.png" alt="discover"/>
 		        <p>
 		        	Set up an account with us and begin <span class = "text-success">promoting and selling within minutes</span>. A small deposit will be taken to protect the community and ensure the <span class = "text-success">trust of the user-base</span>. 
 		        </p>
 		    </div>
 		    <div class="span3">
 		        <h2>Promote</h2>
+		        <img class="image-thumbnail" src="images/discover.png" alt="discover"/>
 		        <p>
 		        	Promote your event to the extent that <span class = "text-success">suits your marketing and PR budget</span>. Use our unique advertising features to ensure your event is <span class = "text-success">seen by your target market</span>. Narrow your audience by interest, what they like to talk about, what event types they have been to and what they have tickets for already. This list is by no means exhaustive and <span class = "text-success">we would love to have your input</span>.
 		        </p>
 		    </div>
 		    <div class="span3">
 		        <h2>Entertain</h2>
+		        <img class="image-thumbnail" src="images/discover.png" alt="discover"/>
 		        <p>
 		        	<span class = "text-success">Sell tickets with ease</span> and have all payment and commission systems taken care of by us. Use traditional NFC (near field communication) readers or <span class = "text-success">ticket check with Android tablets</span> at the door. Ensure only the devices you allow to be used are actually used at checking time to <span class = "text-success">prevent ticket fraud</span>. When all is done, browse our <span class = "text-success">event report to see comments on what users thought about it</span> so as to improve future events.
 		        </p>
 		    </div>
 		    <div class = "span3">
 		        <h2>Get Paid!</h2>
+		        <img class="image-thumbnail" src="images/get_paid.png" alt="discover"/>
 		        <p>
 		        	On completion of the event, have the <span class = "text-success">money directly transferred to your bank</span> account. If you wish to avoid nasty bank fees we can deal in <span class = "text-success">Paypal or Stripe</span> account systems to facilitate your needs. Anything else that would make your life easier? <span class = "text-success">Please call and we'll help!</span>
 		        </p>
